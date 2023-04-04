5b5cbf9217adfa7d73e56978bd20f4a31d8f4e2d
massa <zoumaniguimassa12@gmail.com>
2023-04-04 17:23:17 +0000

    Update .eslintrc.json

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,27 +1,27 @@
 const path = require('path');
 
 module.exports = {
- mode: 'development',
- // rest of the config
+  mode: 'development',
+  // rest of the config
   entry: './src/index.js',
   output: {
-   filename: 'bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
-   rules: [
-     {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-   ],
- },
- devServer: {
-  static: {
-    directory: path.join(__dirname, 'dist'),
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
-},
-optimization: {
- runtimeChunk: 'single',
-},
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
